f9445ef314bb6a8a62bdbde2856429b8faadd6ae
vivian-catao <59140869+vivian-catao@users.noreply.github.com>
2022-02-03 20:48:29 -0300

    first_cascade test

--- inicio.py
+++ inicio.py
@@ -1,16 +1,17 @@
 import cv2
 
-obj_img = cv2.imread("maispessoas.jpg") #, cv2.IMREAD_COLOR 
+obj_img = cv2.imread("test_10.png") #, cv2.IMREAD_COLOR 
 obj_img_cinza = cv2.cvtColor(obj_img, cv2.COLOR_BGR2GRAY)
 
-detector_facial = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
-deteccoes = detector_facial.detectMultiScale(obj_img_cinza, scaleFactor=1.2, minNeighbors = 5 )
+
+classificador = cv2.CascadeClassifier('first_cascade.xml') 
+deteccoes = classificador.detectMultiScale(obj_img_cinza, scaleFactor=1.1, minNeighbors = 5 )
 print(deteccoes)
 print (len(deteccoes))
 
 for x, y,l,h in deteccoes:
     #print(x,y,l,h)
-    cv2.rectangle(obj_img,(x,y),(x+l,y+h),(0,255,0),5)
+    cv2.rectangle(obj_img,(x,y),(x+l,y+h),(0,255,0),2)
 
 
 
